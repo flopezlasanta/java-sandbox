eb6142510e5312a75d1813048ffd3e11af4fe56a
teraflopx <teraflopx@gmail.com>
2015-03-28 11:04:14 +0100

    Added a few more algorithms: quicksort, permutations, mergesort

--- src/main/java/org/teraflopx/jsandboox/algorithm/ArrayOperations.java
+++ src/main/java/org/teraflopx/jsandboox/algorithm/ArrayOperations.java
@@ -1,22 +1,25 @@
 package org.teraflopx.jsandboox.algorithm;
 
+import java.util.Arrays;
+
+// utility class
 public class ArrayOperations {
 
+  // no need to instantiate
   private ArrayOperations() {}
 
-  // TODO
-  public static void quickSort() {}
+  // quicksort algorithm
+  public static void quickSort(int[] data) {
+    quickSort(data, 0, data.length - 1);
+  }
 
-  // TODO
-  public static void mergeSort() {}
-
-  // TODO
-  public static boolean isSorted() {
+  // TODO check if an array is sorted or not
+  public static boolean isSorted(int[] data) {
     return true;
   }
 
-  // TODO and return number of rotations
-  public static boolean isSortedRotated() {
+  // TODO ...and return number of rotations
+  public static boolean isSortedRotated(int[] data) {
     return true;
   }
 
@@ -25,7 +28,19 @@ public class ArrayOperations {
 
   }
 
-  // note: assumes the array is sorted
+  // merge sort algorithm
+  public static void mergeSort(int[] data) {
+    int[] helper = new int[data.length]; // a helper is needed!
+    mergeSort(data, helper, 0, data.length - 1);
+    System.out.println(Arrays.toString(data));
+  }
+
+  // obtain all the permutations
+  public static void findPermutations(String str) {
+    permute("", str);
+  }
+
+  // perform a binary search recursive; note: assumes the array is sorted
   public static int binarySearchRecursive(int key, int[] data, int start, int end) {
     if (start > end)
       return -1;
@@ -40,7 +55,7 @@ public class ArrayOperations {
     return middle;
   }
 
-  // note: assumes the array is sorted
+  // perform a binary search linear; note: assumes the array is sorted
   public static int binarySearchLinear(int key, int[] data, int start, int end) {
     while (start < end) {
       int middle = (start + end) / 2;
@@ -55,4 +70,71 @@ public class ArrayOperations {
 
     return (start == end && data[start] == key) ? start : -1;
   }
+
+  private static void quickSort(int[] data, int low, int high) {
+    int i = low, j = high;
+    int pivot = data[low + (high - low) / 2];
+
+    while (i <= j) {
+      while (data[i] < pivot) {
+        i++;
+      }
+      while (data[j] > pivot) {
+        j--;
+      }
+
+      // why not i < j only? because when i == j we have to break the loop by incrementing!
+      if (i <= j) {
+        int tmp = data[i];
+        data[i] = data[j];
+        data[j] = tmp;
+
+        i++;
+        j--;
+      }
+    }
+    // Recursion
+    if (low < j)
+      quickSort(data, low, j);
+    if (i < high)
+      quickSort(data, i, high);
+  }
+
+  private static void permute(String prefix, String str) {
+    if (str.length() == 0)
+      System.out.println(prefix);
+    for (int i = 0; i < str.length(); i++)
+      permute(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1));
+  }
+
+  private static void mergeSort(int[] data, int[] helper, int low, int high) {
+    if (low < high) {
+      int middle = low + (high - low) / 2;
+      mergeSort(data, helper, low, middle);
+      mergeSort(data, helper, middle + 1, high);
+      merge(data, helper, low, middle, high);
+    }
+  }
+
+  private static void merge(int[] data, int[] helper, int low, int middle, int high) {
+    for (int i = low; i <= high; i++) {
+      helper[i] = data[i];
+    }
+
+    int i = low;
+    int x = low;
+    int y = middle + 1;
+    while (x <= middle && y <= high) {
+      if (helper[x] < helper[y])
+        data[i++] = helper[x++];
+      else
+        data[i++] = helper[y++];
+    }
+
+    while (x <= middle)
+      data[i++] = helper[x++];
+  }
 }
+
+// TODO no need to sort / search for empty arrays!
+// TODO add safety measures for wrong input values
--- src/test/java/org/teraflopx/jsandboox/algorithm/ArrayOperationsTest.java
+++ src/test/java/org/teraflopx/jsandboox/algorithm/ArrayOperationsTest.java
@@ -2,6 +2,7 @@ package org.teraflopx.jsandboox.algorithm;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
@@ -9,8 +10,25 @@ import org.junit.Test;
 public class ArrayOperationsTest {
 
   @Test
+  public void testQuickSort() {
+    int[] data = new int[] {2, 8, 6, 4, 9, 5, 7, 3, 10, 1};
+    ArrayOperations.quickSort(data);
+  }
+
+  @Test
+  public void testPermutations() {
+    ArrayOperations.findPermutations("abc");
+  }
+
+  @Test
+  public void testMergeSort() {
+    int[] data = new Random().ints().limit(100).toArray();
+    ArrayOperations.mergeSort(data);
+  }
+
+  @Test
   public void testBinarySearchRecursive() {
-    int[] data = new int[] {1, 2, 6, 7, 8, 12};
+    int[] data = new Random().ints().limit(100).toArray();
 
     long start = System.nanoTime();
 
@@ -19,7 +37,6 @@ public class ArrayOperationsTest {
       assertEquals(ArrayOperations.binarySearchRecursive(data[i], data, 0, data.length - 1), i);
 
     long end = System.nanoTime();
-
     System.out.println("Elapsed time (ms): " + TimeUnit.NANOSECONDS.toMillis(end - start));
   }
 
@@ -34,7 +51,6 @@ public class ArrayOperationsTest {
       assertEquals(ArrayOperations.binarySearchLinear(data[i], data, 0, data.length - 1), i);
 
     long end = System.nanoTime();
-
     System.out.println("Elapsed time (ms): " + TimeUnit.NANOSECONDS.toMillis(end - start));
   }
 }
